a36c56baec5f1459501bb6c869007ddf7087c65b
Angelina <angelina.ivaylova.ivanova04@gmail.com>
2025-07-07 17:42:30 +0300

    Clear the screen on exit

--- kilo.c
+++ kilo.c
@@ -18,6 +18,9 @@ struct termios original;
 /*** terminal ***/
 
 void die(const char* s) {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
     perror(s);
     exit(1);
 }
@@ -73,6 +76,8 @@ void editorProcessKeypress() {
         
     switch(c) {
         case CTRL_KEY('q'):
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
             break;
     }
