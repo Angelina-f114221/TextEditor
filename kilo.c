106e91121b17d4dd94ce77cd0ce6680652d1e465
Angelina <angelina.ivaylova.ivanova04@gmail.com>
2025-07-07 16:41:12 +0300

    Map ctrl + q as an exit operation

--- kilo.c
+++ kilo.c
@@ -7,6 +7,10 @@
 #include <stdlib.h>
 #include <errno.h>
 
+/*** defines ***/
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 /*** date ***/
 
 struct termios original;
@@ -58,7 +62,7 @@ int main() {
         } else {
             printf("%d ('%c')\r\n", c, c);
         }
-        if (c == 'q') {
+        if (c == CTRL_KEY('q')) {
             break;
         }
     }
