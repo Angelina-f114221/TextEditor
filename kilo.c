e509bef6e754aaa966901bee6f2c06dff2f694b1
Angelina <angelina.ivaylova.ivanova04@gmail.com>
2025-07-13 19:59:24 +0300

    Hide cursor and clear lines

--- kilo.c
+++ kilo.c
@@ -146,6 +146,7 @@ void editorDrawRows(struct abuf *ab) {
     for (y = 0; y < E.screenRows; y++) {
         abAppend(ab, "~", 1);
 
+        abAppend(ab, "\x1b[K", 3);
         if (y < E.screenRows - 1) {
             abAppend(ab, "\r\n", 2);
         }
@@ -156,7 +157,6 @@ void editorRefreshScreen() {
     struct abuf ab = ABUF_INIT;
     
     abAppend(&ab, "\x1b[?25l", 6);
-    abAppend(&ab, "\x1b[2J", 4);
     abAppend(&ab, "\x1b[H", 3);
 
     editorDrawRows(&ab);
