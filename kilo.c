ace79307232d1745e9d9d7e1d96a10d9cb973076
Angelina <angelina.ivaylova.ivanova04@gmail.com>
2025-07-22 17:21:15 +0300

    Add check for cursor boundaries

--- kilo.c
+++ kilo.c
@@ -226,16 +226,24 @@ void editorRefreshScreen() {
 void editorMoveCursor(int key) {
     switch(key) {
         case ARROW_LEFT:
+            if (E.cx != 0) {
             E.cx--;
+            }
             break;
         case ARROW_RIGHT:
+            if (E.cx != E.screenCols - 1) {
             E.cx++;
+            }
             break;
         case ARROW_UP:
+            if (E.cy != 0) {
             E.cy--;
+            }
             break;
         case ARROW_DOWN:
+            if (E.cy != E.screenRows - 1) {
             E.cy++;
+            }
             break;
     }
 }
