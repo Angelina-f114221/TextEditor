0ea0fb30b35bdab8127fde2f2419ea189f92138d
Angelina <angelina.ivaylova.ivanova04@gmail.com>
2025-07-24 13:45:21 +0300

    Display miltiple lines

--- kilo.c
+++ kilo.c
@@ -48,7 +48,7 @@ struct editorConfig
     int screenCols;
     int cx, cy;
     int numrows;
-    erow row;
+    erow* row;
 };
 
 struct editorConfig E;
@@ -243,6 +243,19 @@ int getWindowSize(int *rows, int *cols)
     }
 }
 
+/*** row operations ***/
+
+void editorAppendRow(char* s, size_t len) {
+    E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+
+    int at = E.numrows;
+    E.row[at].size = len;
+    E.row[at].chars = malloc(len + 1);
+    memcpy(E.row[at].chars, s, len);
+    E.row[at].chars[len] = '\0';
+    E.numrows++;
+}
+
 /*** file i/o ***/
 
 void editorOpen(char *filename)
@@ -256,18 +269,15 @@ void editorOpen(char *filename)
     char *line = NULL;
     size_t linecap = 0;
     ssize_t linelen;
-    linelen = getline(&line, &linecap, fp);
-    if (linelen != -1)
+    
+    while ((linelen = getline(&line, &linecap, fp)) != -1)
     {
         while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r'))
         {
             linelen--;
         }
-        E.row.size = linelen;
-        E.row.chars = malloc(linelen + 1);
-        memcpy(E.row.chars, line, linelen);
-        E.row.chars[linelen] = '\0';
-        E.numrows = 1;
+        editorAppendRow(line, linelen);
+        
     }
     free(line);
     fclose(fp);
@@ -337,12 +347,12 @@ void editorDrawRows(struct abuf *ab)
         }
         else
         {
-            int len = E.row.size;
+            int len = E.row[y].size;
             if (len > E.screenCols)
             {
                 len = E.screenCols;
             }
-            abAppend(ab, E.row.chars, len);
+            abAppend(ab, E.row[y].chars, len);
         }
 
         abAppend(ab, "\x1b[K", 3);
@@ -448,6 +458,7 @@ void initEditor()
     E.cx = 0;
     E.cy = 0;
     E.numrows = 0;
+    E.row = NULL;
 
     if (getWindowSize(&E.screenRows, &E.screenCols) == -1)
     {
