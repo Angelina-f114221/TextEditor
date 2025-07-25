ad16818d314c7b2ae49e92cb11e8d2dec225da9c
Angelina <angelina.ivaylova.ivanova04@gmail.com>
2025-07-25 14:28:18 +0300

    Add safety check for end of file

--- kilo.c
+++ kilo.c
@@ -408,6 +408,7 @@ void editorRefreshScreen()
 
 void editorMoveCursor(int key)
 {
+    erow* row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
     switch (key)
     {
     case ARROW_LEFT:
@@ -417,7 +418,10 @@ void editorMoveCursor(int key)
         }
         break;
     case ARROW_RIGHT:
-            E.cx++; break;
+        if (row && E.cx < row->size) {
+            E.cx++;
+        }
+        break;
     case ARROW_UP:
         if (E.cy != 0)
         {
